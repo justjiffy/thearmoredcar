70d78954cb430d07c51e2d1de5de1b65d9f36e14
Juli Troy <justjiffy@gmail.com>
2025-02-01 08:16:32 +0000

    js to manage global elements

---
+++ controls.js
@@ -0,0 +1,8 @@
+// Create the left arrow element
+const spanL = document.createElement('span');
+spanL.innerHTML = "<";
+spanL.classList.add("arrow");
+spanL.classList.add("left");
+
+// Append the spanL to the body of the document
+document.body.appendChild(spanL);
---
+++ script.js
@@ -0,0 +1,8 @@
+// Create the footer element
+const footer = document.createElement('footer');
+footer.innerHTML = `
+  <p>Copyright 2025. All Rights Reserved</p>
+`;
+
+// Append the footer to the body of the document
+document.body.appendChild(footer);
